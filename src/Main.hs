31eb649d51f8daeba237d4a71917211ecb05fe16
Vitor Rodrigues <vitor@di.unito.it>
2016-06-21 12:11:58 +0200

    remove dist files

--- src/Main.hs
+++ src/Main.hs
@@ -18,13 +18,19 @@ module Main (
 ) where
 
 import Code
-import Control.Monad (unless)
+import Control.Monad
 import Data.List (stripPrefix)
 import System.Exit (exitFailure)
 import Test.QuickCheck.All (quickCheckAll)
 import NMParser
 import Text.Parsec.String
 import Data.Algorithm.Diff3
+import Python
+import System.Process
+import System.Exit
+import Control.Exception
+import Path
+import Data.String.Utils
 
 -- Simple function to create a hello message.
 hello s = "Hello " ++ s
@@ -43,28 +49,44 @@ testMain = do
     unless allPass exitFailure
 
 
+readDeltaRPM :: String -> IO (String,String)
+readDeltaRPM = defVV $
+                "def export(x): from deltarpm import readDeltaRPM;" ++
+                "d = readDeltaRPM (x);" ++
+                "d1 = d['old_nevr'];" ++
+                "d2 = d['nevr'];" ++
+                "return (d1,d2)"
+
+extractSyms
+    :: String ->
+       IO [(String,String)]
+extractSyms rpm
+    = do
+      let cmd1 = "cd data; rm -rf ./usr; rpm2cpio " ++ rpm ++ ".i686.rpm" ++ " | cpio --quiet -idmv"
+      (ExitSuccess, stdout, stderr) <- readCreateProcessWithExitCode (shell cmd1) ""
+      putStrLn ("stdout :" ++ stdout) >> putStrLn ("stderr :" ++ stderr)
+      b <- (parseRelFile . rstrip) stderr
+      let cmd2 = "cd data; nm " ++ fromRelFile b
+      (ExitSuccess, stdout, stderr) <- readCreateProcessWithExitCode (shell cmd2) ""
+      let Right symbols = parse nmParser stdout
+      return $ map (\(a,b,c) -> (b,c)) symbols
+      where
+      fillout s = case s of { "" -> replicate 8 ' ' ; _ -> s }
+
+
 parser_main = do
-              symbols_europe <- parseFromFile nmParser "./data/symbols_europe.txt"
-              symbols_russia <- parseFromFile nmParser "./data/symbols_russia.txt"
-              let europa = case symbols_europe of
-                                    --Right syms -> map (\(a,b,c)-> (format a ++ " " ++ b ++ " " ++ c)) syms
-                                    Right syms -> map (\(a,b,c)-> c) syms
-                                    Left err -> error (show err)
-              let russia = case symbols_russia of
-                                    --Right syms -> map (\(a,b,c)-> (format a ++ " " ++ b ++ " " ++ c)) syms
-                                    Right syms -> map (\(a,b,c)-> c) syms
-                                    Left err -> error (show err)
-              --mapM (putStrLn . show) (zip europe russia)
-              let h = diff3 europa europa russia
-              {-let f h = case h of
-                            LeftChange a -> putStrLn (show a)
-                            RightChange a -> putStrLn (show a)
-                            Unchanged a -> putStrLn (show a)
-                            Conflict a b c -> putStrLn (a ++ "->" ++ b ++ "->" ++ c) -}
-              mapM (\hunk -> putStrLn (show hunk)) (filter (\hunk -> case hunk of { RightChange _ -> True; _ -> False } ) h)
+              (old, new) <- readDeltaRPM "./data/ecall-delta-1.0-1.drpm"
+              putStrLn ("old RPM: " ++ old)
+              putStrLn ("new RPM: " ++ new)
+              old_symbols <- extractSyms old
+              putStrLn ("old symbols: " ++ show old_symbols)
+              new_symbols <- extractSyms new
+              putStrLn ("new symbols: " ++ show new_symbols)
+              let h  = diff3 old_symbols old_symbols new_symbols
+                  h' = filter (\hunk -> case hunk of { RightChange _ -> True; _ -> False } ) h
+              mapM (putStrLn . show) h'
               return ()
-              where
-                format s = case s of { "" -> replicate 8 ' ' ; _ -> s }
+
 
 
 -- This is a clunky, but portable, way to use the same Main module file
@@ -72,7 +94,7 @@ parser_main = do
 -- MAIN_FUNCTION is preprocessor macro set to exeMain or testMain.
 -- That way we can use the same file for both an application and for tests.
 #ifndef MAIN_FUNCTION
-#define MAIN_FUNCTION parser_main
+#define MAIN_FUNCTION  parser_main
 #endif
 main = MAIN_FUNCTION
 
--- src/NMParser.hs
+++ src/NMParser.hs
@@ -18,16 +18,16 @@ symbol_name = Parsec.many $
 
 test1 = parse symbol_name "b_@j_"
 
-line2 :: Parsec.Parsec String () (String,String, String)
-line2 = do
+rule1 :: Parsec.Parsec String () (String,String, String)
+rule1 = do
         Parsec.spaces
         sym_type <- Parsec.many1 Parsec.letter
         Parsec.spaces
         sym_name <- symbol_name
         return ("", sym_type, sym_name)
 
-line3 :: Parsec.Parsec String () (String,String, String)
-line3 = do
+rule2 :: Parsec.Parsec String () (String,String, String)
+rule2 = do
         sym_value <- Parsec.count 8 Parsec.alphaNum
         Parsec.spaces
         sym_type <- Parsec.many1 Parsec.letter
@@ -35,6 +35,6 @@ line3 = do
         sym_name <- symbol_name
         return (sym_value, sym_type, sym_name)
 
-nmParser = Parsec.many1 (do { Parsec.try (line3 <|> line2) >>= \line -> Parsec.char '\n' >> return line} )
+nmParser = Parsec.many1 $ do {Parsec.try (rule2 <|> rule1) >>= \line -> Parsec.char '\n' >> return line}
 
 test2 = parse nmParser "08048610 T __x86.get_pc_thunk.bx"
