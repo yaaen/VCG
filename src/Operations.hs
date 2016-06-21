f933cf73b22f3b9c238749ff914f7af19c3b41f8
Vitor Rodrigues <vitor@di.unito.it>
2016-06-21 12:27:44 +0200

    add Operations file

---
+++ src/Operations.hs
@@ -0,0 +1,49 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Operations
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Operations (readDeltaRPM, readRPMSymbols) where
+
+import Text.Parsec.String
+import Python
+import System.Process
+import System.Exit
+import Control.Exception
+import Path
+import Data.String.Utils
+import NMParser
+
+readDeltaRPM :: String -> IO (String,String)
+readDeltaRPM = defVV $
+                "def export(x): from deltarpm import readDeltaRPM;" ++
+                "d = readDeltaRPM (x);" ++
+                "d1 = d['old_nevr'];" ++
+                "d2 = d['nevr'];" ++
+                "return (d1,d2)"
+
+readRPMSymbols
+    :: String ->
+       IO [(String,String)]
+readRPMSymbols rpm
+    = do
+      let cmd1 = "cd data; rm -rf ./usr; rpm2cpio " ++ rpm ++ ".i686.rpm" ++ " | cpio --quiet -idmv"
+      (ExitSuccess, stdout, stderr) <- readCreateProcessWithExitCode (shell cmd1) ""
+      putStrLn ("stdout :" ++ stdout) >> putStrLn ("stderr :" ++ stderr)
+      b <- (parseRelFile . rstrip) stderr
+      let cmd2 = "cd data; nm " ++ fromRelFile b
+      (ExitSuccess, stdout, stderr) <- readCreateProcessWithExitCode (shell cmd2) ""
+      let Right symbols = parse nmParser stdout
+      return $ map (\(a,b,c) -> (b,c)) symbols
+      where
+      fillout s = case s of { "" -> replicate 8 ' ' ; _ -> s }
+
